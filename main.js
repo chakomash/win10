c0123fd933930a1f9230a7513e6e6b6b8dfcc38c
Adersh Aniyan <56914963+chakomash@users.noreply.github.com>
2020-06-01 00:40:13 +0530

    new files added

---
+++ main.js
@@ -0,0 +1,30 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBoYMFbbcaaIMErRc4cUGbctBgrlEEvrlM",
+    authDomain: "site-ec937.firebaseapp.com",
+    databaseURL: "https://site-ec937.firebaseio.com",
+    projectId: "site-ec937",
+    storageBucket: "site-ec937.appspot.com",
+    messagingSenderId: "733412568056",
+    appId: "1:733412568056:web:bc00405770682033ed440d",
+    measurementId: "G-K3T0B8GM5R"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+var messagesRef = firebase.database().ref('contactformmessages');
+
+$('#contactForm').submit(function(e) {
+    e.preventDefault();
+ 
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: $('.fullname').val(),
+        email: $('.email').val(),
+        subject: $('.subject').val(),
+        message: $('.message').val()
+    });
+ 
+    $('.success-message').show();
+ 
+    $('#contactForm')[0].reset();
+});
